49babd308757dccb567647a59bdf21f3cddc75b2
Nicholas Silas <njsilas@gmail.com>
2021-02-15 15:40:43 -0500

    set up backend, gemfile updated

---
+++ backend/db/migrate/20210215203202_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.string :nat
+      t.string :pos
+      t.belongs_to :team
+      t.timestamps
+    end
+  end
+end
---
+++ backend/notes.md
@@ -0,0 +1,4 @@
+# to do list   
+# create 2 classes, one team class one players class.
+# team class has many players. atttrs are name
+# players belong to team. attrs are name nationality and position
